ed0e806f04675ae687b706423b8870dd6529f271
Zilenc3 <dwayne.alex@uoit.net>
2015-06-15 23:07:37 -0400

    6.4

--- spec/features/creating_tickets_spec.rb
+++ spec/features/creating_tickets_spec.rb
@@ -1,16 +1,26 @@
 require "rails_helper"
 RSpec.feature "Users can create new tickets" do
+  let(:user) { FactoryGirl.create(:user) }
+
   before do
     project = FactoryGirl.create(:project, name: "Internet Explorer")
     visit project_path(project)
     click_link "New Ticket"
+    login_as(user)
   end
+
   scenario "with valid attributes" do
     fill_in "Name", with: "Non-standards compliance"
     fill_in "Description", with: "My pages are ugly!"
+
     click_button "Create Ticket"
+
     expect(page).to have_content "Ticket has been created."
+    within("#ticket") do
+      expect(page).to have_content "Author: #{user.email}"
+    end
   end
+
   scenario "when providing invalid attributes" do
     click_button "Create Ticket"
     expect(page).to have_content "Ticket has not been created."
